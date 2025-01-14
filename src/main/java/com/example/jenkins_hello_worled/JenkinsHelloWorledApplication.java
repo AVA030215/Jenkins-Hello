9fc03c140b36569d638ee08f3ff9c1ea6ffc3000
AVA030215 <avalee0215@gmail.com>
2025-01-14 11:34:33 +0900

    install gradle

---
+++ src/main/java/com/example/jenkins_hello_worled/JenkinsHelloWorledApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_hello_worled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelloWorledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsHelloWorledApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins_hello_worled/JenkinsHelloWorledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_hello_worled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHelloWorledApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
